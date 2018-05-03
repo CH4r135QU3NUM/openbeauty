ccd4ed5e77e6b901f08ae1805f2d7b5a2c8fb869
charles <charles.quenum@gmail.com>
2018-05-03 23:21:37 +0200

    Push Heroku

--- copycat/config/routes.rb
+++ copycat/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'users#home'
+  get '/', to: 'users#home' 
+  get '/new', to: 'users#new', as: 'inscription'
+  post '/new', to: 'users#create'
+  get '/login', to: 'users#login'
+  post '/login', to: 'users#check'
+  get '/users/:id', to: 'users#show', as:'user'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ copycat/spec/factories/events.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :event do
+    description "MyText"
+    date "2018-05-03 21:53:29"
+    place "MyString"
+  end
+end
